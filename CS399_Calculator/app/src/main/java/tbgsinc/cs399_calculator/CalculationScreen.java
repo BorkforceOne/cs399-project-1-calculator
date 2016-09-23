91037735816be08be2dd242ec8201401183e935c
TannerStevens <tanner.stevens42@gmail.com>
2016-09-23 15:50:47 -0700

    Buttons with working onTap event

--- CS399_Calculator/app/src/main/java/tbgsinc/cs399_calculator/CalculationScreen.java
+++ CS399_Calculator/app/src/main/java/tbgsinc/cs399_calculator/CalculationScreen.java
@@ -2,6 +2,9 @@ package tbgsinc.cs399_calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CalculationScreen extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class CalculationScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation_screen);
+    }
+
+    public void onTap(View v){
+        Button b = (Button) findViewById(v.getId());
+        TextView tv = (TextView) findViewById(R.id.equationView);
+
+        tv.setText(tv.getText() + (String)b.getText());
     }
 }
