9be9fb9168358ac9680c1696882b2f7e129ec786
TannerStevens <tanner.stevens42@gmail.com>
2016-09-23 17:53:59 -0700

    Implemented Grid Layout & Added Parser Class

---
+++ CS399_Calculator/app/src/main/java/tbgsinc/cs399_calculator/Operator.java
@@ -0,0 +1,58 @@
+package tbgsinc.cs399_calculator;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Operator {
+	EXPONENTIATION ("^", 2),
+	MULTIPLICATION ("*", 3),
+	DIVISION ("/", 3),
+	ADDITION ("+", 4),
+	SUBTRACTION ("-", 4),
+	L_PARENTHESIS ("(", 5),
+	R_PARENTHESIS (")", 5),
+	SIN ("sin", 1),
+	COS ("cos", 1),
+	TAN ("tan", 1),
+	LOG ("log", 1);
+	
+	private static final Map<String, Operator> operators;
+	static{
+		Map<String, Operator> ops = new HashMap<String, Operator>();
+		ops.put(Operator.EXPONENTIATION.symbol(), Operator.EXPONENTIATION);
+		ops.put(Operator.MULTIPLICATION.symbol(), Operator.MULTIPLICATION);
+		ops.put(Operator.DIVISION.symbol(), Operator.DIVISION);
+		ops.put(Operator.ADDITION.symbol(), Operator.ADDITION);
+		ops.put(Operator.SUBTRACTION.symbol(), Operator.SUBTRACTION);
+		ops.put(Operator.L_PARENTHESIS.symbol(), Operator.L_PARENTHESIS);
+		ops.put(Operator.R_PARENTHESIS.symbol(), Operator.R_PARENTHESIS);
+		ops.put(Operator.SIN.symbol(), Operator.SIN);
+		ops.put(Operator.COS.symbol(), Operator.COS);
+		ops.put(Operator.TAN.symbol(), Operator.TAN);
+		ops.put(Operator.LOG.symbol(), Operator.LOG);
+		operators = Collections.unmodifiableMap(ops);
+	}
+	
+	private final String symbol;
+	private final int precedence;
+	Operator(String symbol, int precedence){
+		this.symbol = symbol;
+		this.precedence = precedence;
+	}
+	public String symbol(){return symbol;}
+	public int precedence(){return precedence;}
+	
+	public static int findPrecedence(char c){
+		return operators.get(String.valueOf(c)).precedence();
+	}
+	public static int findPrecedence(String c){
+		return operators.get(c).precedence();
+	}
+	public static boolean isOperator(char c){
+		return operators.containsKey(String.valueOf(c));
+	}
+	public static boolean isOperator(String c){
+		return operators.containsKey(c);
+	}
+}
---
+++ CS399_Calculator/app/src/main/java/tbgsinc/cs399_calculator/Parser.java
@@ -0,0 +1,107 @@
+package tbgsinc.cs399_calculator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Parser {
+	private Stack<String> operators = new Stack<String>();
+	private String outStr;
+	
+	public Parser(){}
+	
+	private void foundOperator(String op){
+		if(operators.empty()){
+			operators.push(op);
+			return;
+		}
+		String sTop = operators.peek();
+		if(sTop.equals("(") && !op.equals(")")) 
+			operators.push(op);
+		else if(op.equals("("))
+			operators.push(op);
+		else if(op.equals(")")){
+			String tempS = operators.pop();
+			while (!tempS.equals("(")){
+				outStr+=" "+tempS;
+				tempS = operators.pop();
+			}
+		}
+		else{
+			while(true){
+				if(operators.empty()){
+					operators.push(op);
+					break;
+				}
+				
+				sTop = operators.peek();
+				if(Operator.findPrecedence(op)<Operator.findPrecedence(sTop)){
+					operators.push(op);
+					break;
+				}
+				else if(Operator.findPrecedence(op)==Operator.findPrecedence(sTop)){
+					//Determine Association
+					if(op=="^"){//Right Associative
+						operators.push(op);
+					}
+					else{//Left Associative
+						outStr+=" "+operators.pop();
+						operators.push(op);
+					}
+					break;
+				}
+				else if(Operator.findPrecedence(op)>Operator.findPrecedence(sTop)){
+					outStr+=" "+operators.pop();
+				}
+			}
+		}
+	}
+	public String translate(String eq){
+		if(eq.charAt(eq.length()-1)!=')' && Operator.isOperator(eq.charAt(eq.length()-1)))
+			return eq; //Already Postfix
+		
+		operators.clear();
+		outStr = "";
+		
+		String op = "";
+		for(int i=0;i<eq.length();i++){
+			char c = eq.charAt(i);
+			if(Operator.isOperator(op)){
+				foundOperator(op);
+				op="";
+				i--; //Decrement i, since c will not be apart op, thus c's iteration is interrupted by op
+			}
+			else if(Operator.isOperator(c)){
+				if(op.length()>0){
+					outStr+=" "+op;
+					op="";
+				}
+				foundOperator(""+c);
+			}
+			else op+=c;
+		}
+		if(op.length()>0) outStr+=" "+op;
+		
+		while(!operators.empty()){
+			outStr+=" "+operators.pop();
+		}
+		
+		return outStr.trim();
+	}
+	public ArrayList<String> parseFile(File inputFile) throws IOException{
+		ArrayList<String> lineList = new ArrayList<String>();
+		BufferedReader br = new BufferedReader(new FileReader(inputFile));
+		String line = null;
+		while((line = br.readLine()) != null) {
+			lineList.add(line);
+		}
+		br.close();
+		return lineList;
+	}
+}
